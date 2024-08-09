7d828ea95847e52fe946295fe6fd4f126d42d286
Alashiba <141456856+Alashiba@users.noreply.github.com>
2024-08-09 22:09:55 +0800

    ver2

--- assignment/index.php
+++ assignment/index.php
@@ -1,27 +1,40 @@
 <?php session_start(); ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title>Homepage</title>
-	<link href="style.css" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link href="stylesindex.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
-	<div id="header">
-		Welcome to my page!
-	</div>
-	<?php
-	if(isset($_SESSION['valid'])) {			
-		include("connection.php");					
-		$result = mysqli_query($mysqli, "SELECT * FROM User");
-	?>	
-		Welcome <?php echo $_SESSION['username'] ?> ! <a href='logout.php'>Logout</a><br/>
-	<?php	
-	} else {
-		echo "You must be logged in to view this page.<br/><br/>";
-		echo "<a href='login.php'>Login</a> | <a href='register.php'>Register</a>";
-	}
-	?>
-	<div id="footer">
-	</div>
+    <header id="header">
+        <div class="container">
+            <h1>Welcome to My Page!</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <?php
+            if (isset($_SESSION['valid'])) {
+                include("connection.php");                    
+                $result = mysqli_query($mysqli, "SELECT * FROM User");
+            ?>  
+                <p>Welcome, <strong><?php echo $_SESSION['username']; ?></strong>! <a href='logout.php' class="button logout">Logout</a></p>
+            <?php   
+            } else {
+                echo "<p>You must be logged in to view this page.</p>";
+                echo "<a href='login.php' class='button'>Login</a> | <a href='register.php' class='button'>Register</a>";
+            }
+            ?>
+        </div>
+    </main>
+
+    <footer id="footer">
+        <div class="container">
+            <p>&copy; 2024 My Website. All Rights Reserved.</p>
+        </div>
+    </footer>
 </body>
 </html>
